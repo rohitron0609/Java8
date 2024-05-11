20f6162d02dc1e1dad5149d98d5ab848effe9b58
rohit <rohitron0609@gmail.com>
2024-05-11 17:41:04 +0530

    Initial Commit. 1) Odd numbers from list.

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        List<Integer> input = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        System.out.print(oddNumbers(input));
+    }
+
+    public static List<Integer> oddNumbers(List<Integer> input){
+        return input.stream().filter(n -> (n & 1) == 1).collect(Collectors.toList());
+    }
+}
