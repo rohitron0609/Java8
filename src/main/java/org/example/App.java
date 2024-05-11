6a5efac3eb2fafaf50c1448964730c168cfbab1f
rohit <rohitron0609@gmail.com>
2024-05-11 22:18:30 +0530

    3) remove dupes

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -11,9 +11,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        List<Integer> input = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        List<Integer> input = Arrays.asList(1,2,2,4,8,3,4,5,6,7,8,9,10);
         System.out.print(oddNumbers(input));
         System.out.println(evenNumbers(input));
+        System.out.println(removeDupes(input));
     }
 
     public static List<Integer> oddNumbers(List<Integer> input){
@@ -22,5 +23,9 @@ public class App
 
     public static List<Integer> evenNumbers(List<Integer> input){
         return input.stream().filter(n -> (n & 1) == 0).collect(Collectors.toList());
+    }
+
+    public static List<Integer> removeDupes(List<Integer> input){
+        return input.stream().distinct().collect(Collectors.toList());
     }
 }
