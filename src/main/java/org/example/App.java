7dd48101035941f649e774ee2a6acbe5f5500328
rohit <rohitron0609@gmail.com>
2024-05-11 17:44:35 +0530

    2) even numbers from list.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -13,9 +13,14 @@ public class App
     {
         List<Integer> input = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
         System.out.print(oddNumbers(input));
+        System.out.println(evenNumbers(input));
     }
 
     public static List<Integer> oddNumbers(List<Integer> input){
         return input.stream().filter(n -> (n & 1) == 1).collect(Collectors.toList());
+    }
+
+    public static List<Integer> evenNumbers(List<Integer> input){
+        return input.stream().filter(n -> (n & 1) == 0).collect(Collectors.toList());
     }
 }
