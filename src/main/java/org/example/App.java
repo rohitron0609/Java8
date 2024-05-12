8a3be835edaec5b82b449f269bd9d606084b7307
rohit <rohitron0609@gmail.com>
2024-05-12 16:59:35 +0530

    4. Elements frequency in a list

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -18,21 +18,37 @@ public class App
 //        System.out.println(evenNumbers(input));
 //        System.out.println(removeDupes(input));
         System.out.println(charFreq("Java Concept Of The Day"));
+        System.out.println(elementFreq(Arrays.asList("Pen", "Eraser", "Note Book", "Pen", "Pencil", "Stapler", "Note Book", "Pencil")));
     }
 
     public static List<Integer> oddNumbers(List<Integer> input){
-        return input.stream().filter(n -> (n & 1) == 1).collect(Collectors.toList());
+        return input.stream()
+                .filter(n -> (n & 1) == 1)
+                .collect(Collectors.toList());
     }
 
     public static List<Integer> evenNumbers(List<Integer> input){
-        return input.stream().filter(n -> (n & 1) == 0).collect(Collectors.toList());
+        return input.stream()
+                .filter(n -> (n & 1) == 0)
+                .collect(Collectors.toList());
     }
 
     public static List<Integer> removeDupes(List<Integer> input){
-        return input.stream().distinct().collect(Collectors.toList());
+        return input.stream()
+                .distinct()
+                .collect(Collectors.toList());
     }
 
     public static Map<Character, Long> charFreq(String input){
-        return input.chars().mapToObj(c -> (char) c).collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
+        return input.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
     }
+
+    public static Map<String, Long> elementFreq(List<String> input){
+        return input
+                .stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    }
+
 }
