66356cd1492532fb8302d67923826223db7c3dbc
rohit <rohitron0609@gmail.com>
2024-05-12 16:15:42 +0530

    3. Char frequency in a string

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import java.sql.SQLOutput;
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /**
@@ -12,9 +14,10 @@ public class App
     public static void main( String[] args )
     {
         List<Integer> input = Arrays.asList(1,2,2,4,8,3,4,5,6,7,8,9,10);
-        System.out.print(oddNumbers(input));
-        System.out.println(evenNumbers(input));
-        System.out.println(removeDupes(input));
+//        System.out.print(oddNumbers(input));
+//        System.out.println(evenNumbers(input));
+//        System.out.println(removeDupes(input));
+        System.out.println(charFreq("Java Concept Of The Day"));
     }
 
     public static List<Integer> oddNumbers(List<Integer> input){
@@ -27,5 +30,9 @@ public class App
 
     public static List<Integer> removeDupes(List<Integer> input){
         return input.stream().distinct().collect(Collectors.toList());
+    }
+
+    public static Map<Character, Long> charFreq(String input){
+        return input.chars().mapToObj(c -> (char) c).collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
     }
 }
